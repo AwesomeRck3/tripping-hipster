c4f54ba4addf7a1508ff2fc9d828399868fe082c
erikdaenitz <erikdaenitz@172.30.154.68>
2015-10-31 19:01:18 -0600

    Updated

---
+++ LearningDone/L2E224.h
@@ -0,0 +1,17 @@
+/*
+ * L2E224.h
+ *
+ *  Created on: Oct 31, 2015
+ *      Author: erikdaenitz
+ */
+
+#ifndef L2E224_H_
+#define L2E224_H_
+
+class L2E224 {
+public:
+	L2E224();
+	virtual ~L2E224();
+};
+
+#endif /* L2E224_H_ */
