a5e0312e91ff090f9e4e5643b9d4e8d7c50060cf
erikdaenitz <erikdaenitz@192.168.0.4>
2015-11-11 18:40:38 -0700

    Bug fixing

---
+++ L2E21BugBuster.cpp
@@ -0,0 +1,24 @@
+/*
+ * L2E21BugBuster.cpp
+ *
+ *  Created on: Nov 11, 2015
+ *      Author: erikdaenitz
+ */
+
+#include "L2E21BugBuster.h"
+#include <iostream>
+
+L2E21BugBuster::L2E21BugBuster() {
+	// TODO Auto-generated constructor stub
+
+}
+
+L2E21BugBuster::~L2E21BugBuster() {
+	// TODO Auto-generated destructor stub
+}
+
+int main(){
+	std::cout << "Is there a bug here?";
+
+	return 0;
+}
---
+++ L2E21BugBuster.h
@@ -0,0 +1,17 @@
+/*
+ * L2E21BugBuster.h
+ *
+ *  Created on: Nov 11, 2015
+ *      Author: erikdaenitz
+ */
+
+#ifndef L2E21BUGBUSTER_H_
+#define L2E21BUGBUSTER_H_
+
+class L2E21BugBuster {
+public:
+	L2E21BugBuster();
+	virtual ~L2E21BugBuster();
+};
+
+#endif /* L2E21BUGBUSTER_H_ */
