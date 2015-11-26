a0c3dc024bd51bda4f73cf6d7fb1b58958551aaf
erikdaenitz <erikdaenitz@192.168.0.4>
2015-11-25 20:22:32 -0700

    Constant variable

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../L3E35.cpp 
+../L3E36.cpp 
 
 OBJS += \
-./L3E35.o 
+./L3E36.o 
 
 CPP_DEPS += \
-./L3E35.d 
+./L3E36.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
